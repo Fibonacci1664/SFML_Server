6ceaac676e491d063a57224a24a91ab0190faa04
Fibonacci <dave.green1664@yahoo.co.uk>
2021-10-08 12:28:32 +0100

    Implemented UDP, msgs are received and echo sent successfully, but client side gets blocked trying to receive echo.

--- SFML_Server/Main.cpp
+++ SFML_Server/Main.cpp
@@ -2,42 +2,37 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
 
-//void testApp()
-//{
-//    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-//    sf::CircleShape shape(100.f);
-//    shape.setFillColor(sf::Color::Green);
-//
-//    while (window.isOpen())
-//    {
-//        sf::Event event;
-//        while (window.pollEvent(event))
-//        {
-//            if (event.type == sf::Event::Closed)
-//                window.close();
-//        }
-//
-//        window.clear();
-//        window.draw(shape);
-//        window.display();
-//    }
-//}
+char dataBuffer[100];
+std::size_t received;
+sf::IpAddress SERVERIP = "127.0.0.1";
+unsigned short SERVERPORT = 4444;
 
+void createTCPListener();
+void createUDP();
+void testApp();
 
 int main()
 {
 	//testApp();
+	//createTCPListener();
+	createUDP();
 
+	return 0;
+}
+
+void createTCPListener()
+{
 	sf::TcpListener tcpListener;
 
 	// Bind the listener to a port
-	if (tcpListener.listen(53000) != sf::Socket::Done)
+	if (tcpListener.listen(SERVERPORT) != sf::Socket::Done)
 	{
 		std::cout << "Bind failed!\n";
 	}
 	else
 	{
-		std::cout << "Bind worked!\n";
+		std::cout << "Bind worked\n";
+		std::cout << "Listening for messages on TCP...\n";
 	}
 
 	// Accept a new connection
@@ -51,5 +46,61 @@ int main()
 		std::cout << "Accept worked!\n";
 	}
 
-	return 0;
+	if (clientSocket.receive(dataBuffer, 100, received) != sf::Socket::Done)
+	{
+		std::cout << "Error receiving using TCP!\n";
+	}
+
+	std::cout << "Received " << received << " bytes\n";
+}
+
+void createUDP()
+{
+	sf::UdpSocket udpServerSocket;
+
+	if (udpServerSocket.bind(SERVERPORT) != sf::Socket::Done)
+	{
+		std::cout << "UDP bind failed!\n";
+	}
+
+	sf::IpAddress clientIP;
+	unsigned short clientPort;
+
+	if (udpServerSocket.receive(dataBuffer, 100, received, clientIP, clientPort) != sf::Socket::Done)
+	{
+		std::cout << "Error receiving using UDP!\n";
+	}
+
+	std::cout << "Received " << received << " bytes from " << SERVERIP << " on port num " << SERVERPORT << '\n';
+
+	// Send data to echo
+	if (udpServerSocket.send(dataBuffer, 100, SERVERIP, SERVERPORT) != sf::Socket::Done)
+	{
+		std::cout << "Error sending echo data by UDP!\n";
+	}
+	else
+	{
+		std::cout << "Msg sent!\n";
+	}
+}
+
+void testApp()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
